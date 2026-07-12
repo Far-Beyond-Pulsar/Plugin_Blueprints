f9edb82ab641dcd38f28f527eed3cb88b4187124
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2026-07-12 03:05:56 -0400

    Update graph.rs

--- src/rendering/graph.rs
+++ src/rendering/graph.rs
@@ -856,10 +856,6 @@ impl NodeGraphRenderer {
                             return;
                         };
 
-                        let frame_uni = GraphUniforms {
-                            viewport: [w as f32, h as f32],
-                            ..uniforms
-                        };
                         canvas.renderer.render_frame(
                                             surface.device(),
                                             surface.queue(),
@@ -867,7 +863,7 @@ impl NodeGraphRenderer {
                                             w,
                                             h,
                                             surface.format(),
-                                            &frame_uni,
+                                            &uniforms,
                                             &comment_instances,
                                             &node_instances,
                                             &wire_instances, // one struct per bezier connection
